53c66ee11b86de672792383b126f3dbecc51bcfc
Phil Lee <phillee@microsoft.com>
2019-06-28 16:49:57 -0700

    update.

--- install-az.ps1
+++ install-az.ps1
@@ -1,2 +1,26 @@
-uninstall-azurerm
-install-module -name az -allowclobber
+###################################################################################################
+#
+# PowerShell configurations
+#
+Set-StrictMode -Version 2.0
+
+# NOTE: Because the $ErrorActionPreference is "Stop", this script will stop on first failure.
+#       This is necessary to ensure we capture errors inside the try-catch-finally block.
+$ErrorActionPreference = "Stop"
+
+# Hide any progress bars, due to downloads and installs of remote components.
+$ProgressPreference = "SilentlyContinue"
+
+# Discard any collected errors from a previous execution.
+$Error.Clear()
+
+# Allow certain operations, like downloading files, to execute.
+Set-ExecutionPolicy Bypass -Scope Process -Force
+
+
+###################################################################################################
+#
+# Main execution block.
+#
+Uninstall-AzureRm
+Install-Module -Name Az -AllowClobber
